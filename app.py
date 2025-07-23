661d2584ba2d48a7bae73baf1d002fdc7987bca6
Kevin Pitts <kevinlpitts@gmail.com>
2025-07-22 17:42:25 -0700

    ✨ UI update: add country flags to frontend display

--- app.py
+++ app.py
@@ -39,3 +39,5 @@ if submitted:
                 st.info("No equivalents found (yet). Try another name.")
         except requests.RequestException as err:
             st.error(f"API error: {err}")
+
+
